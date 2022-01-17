6fb7d19d706a7c9b4ba87f5f5837d520e0131d94
IsaacMito <playblack26@gmail.com>
2022-01-17 15:44:34 -0300

    add

---
+++ import.py
@@ -0,0 +1,8 @@
+import os
+
+os.system("pip install cx_Oracle")
+os.system("pip install opencv-python")
+os.system("pip install clipboard")
+os.system("pip install numpy")
+os.system("pip install Pillow")
+os.system("pip install openpyxl")
